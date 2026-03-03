ae153cec81b650e2dd36fc5428143fd8360ac2b8
filip <filipwidenski1810@gmail.com>
2026-03-03 15:33:01 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,60 @@
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        float a = 5;//lokiec - nadgarstek
-        float b = 3;//lokiec bark
-        float c = 4;//bark nadgarstek
-        double outa = ((b*b) + (c*c) -(a*a))/(2*b*c);
-        outa = Math.acos(outa);
-        outa = Math.toDegrees(outa);
-        System.out.println(outa);
-
-        float noga = 4;
-        float noga2 = 4;
-        float noga3 = 5;
-        double outb = ((noga*noga) + (noga3*noga3) -(noga2*noga2))/(2*noga*noga3);
-        outb = Math.acos(outb);
-        outb = Math.toDegrees(outb);
-        System.out.println(outb);
+                Scanner skaner = new Scanner(System.in);
 
 
-        float biodr1 = 2;
-        float biodr2 = 2;
-        float biodr3 = 2;
-        double outc = ((biodr1*biodr1) + (biodr2*biodr2) -(biodr3*biodr3))/(2*biodr1*biodr1);
-        outc = Math.acos(outc);
-        outc = Math.toDegrees(outc);
-        System.out.println(outc);
+                System.out.println(" Podaj  przedramię, ramię, odległość nadgarstek-bark:");
+                double lokiecA = skaner.nextDouble();
+                double lokiecB = skaner.nextDouble();
+                double lokiecC = skaner.nextDouble();
+
+                double cosLokcia = ((lokiecA * lokiecA) + (lokiecB * lokiecB) - (lokiecC * lokiecC)) / (2 * lokiecA * lokiecB);
+                double katLokcia = Math.toDegrees(Math.acos(cosLokcia));
+
+                System.out.println( Math.round(katLokcia));
+                if (katLokcia >= 90 && katLokcia <= 120) {
+                    System.out.println(" w normie");
+                } else {
+                    System.out.println("Poza normą");
+                }
+
+
+                System.out.println("Podaj tułów, udo, odległość bark-kolano:");
+                double biodroA = skaner.nextDouble();
+                double biodroB = skaner.nextDouble();
+                double biodroC = skaner.nextDouble();
+
+                double cosBiodra = ((biodroA * biodroA) + (biodroB * biodroB) - (biodroC * biodroC)) / (2 * biodroA * biodroB);
+                double katBiodra = Math.toDegrees(Math.acos(cosBiodra));
+
+                System.out.println(Math.round(katBiodra));
+                if (katBiodra >= 90 && katBiodra <= 120) {
+                    System.out.println("w normie");
+                } else {
+                    System.out.println("Poza normą");
+                }
+
+                System.out.println(" Podaj udo, łydka, odległość biodro-kostka:");
+                double kolanoA = skaner.nextDouble();
+                double kolanoB = skaner.nextDouble();
+                double kolanoC = skaner.nextDouble();
+
+                double cosKolana = ((kolanoA * kolanoA) + (kolanoB * kolanoB) - (kolanoC * kolanoC)) / (2 * kolanoA * kolanoB);
+                double katKolana = Math.toDegrees(Math.acos(cosKolana));
+
+                System.out.println(Math.round(katKolana));
+                if (katKolana >= 90 && katKolana <= 110) {
+                    System.out.println("-> Wynik w normie");
+                } else {
+                    System.out.println("-> Poza normą");
+                }
+
+                skaner.close();
+
+
+
 
 
 
