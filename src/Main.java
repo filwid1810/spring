ddfe9a8696a2f009c5da9da015cc359d8db7121f
filip <filipwidenski1810@gmail.com>
2026-03-03 15:03:37 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        float a = 5;//lokiec - nadgarstek
+        float b = 5;//lokiec bark
+        float c = 5;//bark nadgarstek
+        double outa = ((b*b) + (c*c) -(a*a))/(2*b*c);
+        outa = Math.acos(outa);
+        outa = Math.toDegrees(outa);
+        System.out.println(outa);
+
+        double outb = ((a*a) + (c*c) -(b*b))/(2*a*c);
+        outb = Math.acos(outb);
+        outb = Math.toDegrees(outb);
+        System.out.println(outb);
+
+        double outc = ((a*a) + (b*b) -(c*c))/(2*a*b);
+        outc = Math.acos(outc);
+        outc = Math.toDegrees(outc);
+        System.out.println(outc);
 
 
 
